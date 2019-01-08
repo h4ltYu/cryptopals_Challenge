fd254316154168f39ee12727b48470a660a02c7f
h4ltYu <h4ltYu@users.noreply.github.com>
2019-01-08 10:55:57 +0700

    Update c3.py

--- set1/c3.py
+++ set1/c3.py
@@ -1,7 +1,10 @@
+from nostril import nonsense
+import binascii
+import string
 def c3():
-    import binascii
-    import string
     x = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
     tmp = binascii.unhexlify(x)
     for i in string.printable:
-        print (i, ''.join([chr(ord(a)^ord(b)) for (a,b) in zip(tmp, i*len(tmp))]))
+        y = ''.join([chr(ord(a)^ord(b)) for (a,b) in zip(tmp, i*len(tmp))])
+        if(all(c in string.printable for c in y) and not nonsense(y)):
+            print(i,y)
