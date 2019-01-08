8dbf6098517a28454bee96d766de485b4b77665b
h4ltYu <h4ltYu@users.noreply.github.com>
2019-01-08 11:06:12 +0700

    Update c4.py

--- set1/c4.py
+++ set1/c4.py
@@ -7,7 +7,7 @@ def c3(x):
     for i in string.printable:
         y = ''.join([chr(a^ord(b)) for (a,b) in zip(tmp, i*len(tmp))])
         if(all(c in string.printable for c in y) and not nonsense(y)):
-            res.append((i,y))
+            res.append((i,y.strip()))
     return res
 lines = open('4.txt').readlines()
 for i in range(len(lines)):
